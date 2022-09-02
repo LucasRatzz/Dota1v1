5bc23b189f4f4a365269515d50ab721dd33e9c55
Vincenzo <vincenzo.bbosco@gmail.com>
2022-09-01 23:06:27 -0300

    Commit HTML DOM

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 app.use(express.json());
+app.use(cors())
 const port = 3000
 const {createHero} = require('./createHero')
 
